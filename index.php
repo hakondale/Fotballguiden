08540de04b15895d0e4a418044536baa6fc6ce29
hakondale <hakon_dale@hotmail.com>
2014-08-29 17:45:51 +0200

    Header og index

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
-echo "Halla, verden!";
+error_reporting(-1);
+ini_set('display_errors', 'On');
+include "php/html/head.php";
+
+echo "<br> <br> <br> bitches aint shit but hoes and tricks";
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
---
+++ php/html/head.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <title>Fotballguiden</title>
+                	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                        
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" content="text/html; charset=UTF-8">
+                <link href = "/css/bootstrap.css" rel = "stylesheet">
+              <!--  <link href = "css/styles.css" rel = "stylesheet"> -->
+        </head>
+        <body>
+        	<nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+  				<div class="container">
+            <div class="navbar-header">
+      <a class="navbar-brand" href="">Fotballguiden</a>
+    </div>
+  				</div>
+			</nav>
